ac4bae6289d601370951ecf227da6ddf3b970eed
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-03-05 13:13:55 -0300

    Updates

--- src/services/currencyAPI.js
+++
@@ -1,9 +0,0 @@
-const getCurrency = async () => {
-  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
-  const json = await response.json();
-  const data = Object.keys(json);
-  const filtered = data.filter((value) => value !== 'USDT');
-  return filtered;
-};
-
-export default getCurrency;
